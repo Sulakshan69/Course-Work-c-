3800a03b5b3cd1ddb37819dd4967c1438421cc75
Sulakshan69 <168500936+Sulakshan69@users.noreply.github.com>
2025-03-20 16:49:12 +0545

    Update Question no 1.cpp

--- Question no 1.cpp
+++ Question no 1.cpp
@@ -1,4 +1,4 @@
-1.	Write a program that takes a temperature value from the user. It should then allow the user to choose between Celsius (C) and Fahrenheit (F) for conversion. After the user selection, it should then convert the entered temperature to the chosen scale and display the result.
+1.Write a program that takes a temperature value from the user. It should then allow the user to choose between Celsius (C) and Fahrenheit (F) for conversion. After the user selection, it should then convert the entered temperature to the chosen scale and display the result.
 Use appropriate data types for temperature and handle error like non-numeric input. 
 Use the following formula for conversion:
 F = (C x 9/5) + 32
@@ -25,9 +25,12 @@ int main() {
     if (choice == 'C' || choice == 'c') {
         double celsius = (temp - 32) * 5 / 9;
         cout << "Temperature in Celsius: " << celsius << " C" << endl;
+        
     } else if (choice == 'F' || choice == 'f') {
         double fahrenheit = (temp * 9 / 5) + 32;
         cout << "Temperature in Fahrenheit: " << fahrenheit << " F" << endl;
     }
+
+    
     return 0;
 }
