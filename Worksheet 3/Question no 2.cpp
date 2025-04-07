8ebdedefa1b2eb1e60b697f9a1b29acac5130a89
Sulakshan69 <168500936+Sulakshan69@users.noreply.github.com>
2025-04-07 16:54:51 +0545

    Update Question no 2.cpp

--- Worksheet 3/Question no 2.cpp
+++ Worksheet 3/Question no 2.cpp
@@ -1,44 +1,74 @@
+1.	Create a base class Vehicle and two derived classes Car and Bike:
+
+1.	Vehicle has registration number and color
+2.	Car adds number of seats
+3.	Bike adds engine capacity
+4.	Each class should have its own method to write its details to a file
+5.	Include proper inheritance and method overriding
+
+
+
+
 #include <iostream>
-#include <cmath>
+#include <fstream>
 using namespace std;
 
-class Circle {
-private:
-    float radius;
+class Vehicle {
+protected:
+    string regNumber, color;
 
 public:
-    Circle(float r) : radius(r) {}
+    Vehicle(string reg, string col) : regNumber(reg), color(col) {}
 
-    float getArea() const {
-        return M_PI * radius * radius;
+    virtual void writeToFile(ofstream &file) {
+        file << "Vehicle Reg: " << regNumber << ", Color: " << color << endl;
     }
 };
 
-void compareCircles(const Circle &c1, const Circle &c2) {
-    float area1 = c1.getArea();
-    float area2 = c2.getArea();
+class Car : public Vehicle {
+    int seats;
 
-    cout << "Circle 1 Area: " << area1 << endl;
-    cout << "Circle 2 Area: " << area2 << endl;
+public:
+    Car(string reg, string col, int s) : Vehicle(reg, col), seats(s) {}
 
-    if (area1 > area2) {
-        cout << "Circle 1 has a larger area." << endl;
-    } else if (area2 > area1) {
-        cout << "Circle 2 has a larger area." << endl;
-    } else {
-        cout << "Both circles have the same area." << endl;
+    void writeToFile(ofstream &file) override {
+        file << "Car Reg: " << regNumber << ", Color: " << color << ", Seats: " << seats << endl;
     }
-}
+};
+
+class Bike : public Vehicle {
+    int engineCapacity;
+
+public:
+    Bike(string reg, string col, int cap) : Vehicle(reg, col), engineCapacity(cap) {}
+
+    void writeToFile(ofstream &file) override {
+        file << "Bike Reg: " << regNumber << ", Color: " << color << ", Engine Capacity: " << engineCapacity << "cc" << endl;
+    }
+};
 
 int main() {
-    float r1, r2;
-    cout << "Enter radius of first circle: ";
-    cin >> r1;
-    cout << "Enter radius of second circle: ";
-    cin >> r2;
+    string reg, color;
+    int seats, capacity;
 
-    Circle circle1(r1), circle2(r2);
-    compareCircles(circle1, circle2);
+    ofstream file("vehicles.txt");
 
+    cout << "Enter Car details:\n";
+    cout << "Registration Number: "; cin >> reg;
+    cout << "Color: "; cin >> color;
+    cout << "Number of Seats: "; cin >> seats;
+    Car car(reg, color, seats);
+    car.writeToFile(file);
+
+    cout << "\nEnter Bike details:\n";
+    cout << "Registration Number: "; cin >> reg;
+    cout << "Color: "; cin >> color;
+    cout << "Engine Capacity (cc): "; cin >> capacity;
+    Bike bike(reg, color, capacity);
+    bike.writeToFile(file);
+
+    file.close();
+    cout << "\nDetails saved to 'vehicles.txt'." << endl;
     return 0;
 }
+
