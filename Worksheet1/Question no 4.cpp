60d53be91a3fac96b1ea946ea9e8a65fb19896fc
Sulakshan69 <168500936+Sulakshan69@users.noreply.github.com>
2025-04-07 16:13:41 +0545

    Add files via upload

---
+++ Worksheet1/Question no 4.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int day_num;
+    cout << "Enter a number (1-7) to find the day of the week: ";
+
+    if (cin >> day_num) {
+        const char* days[7] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+        if (day_num >= 1 && day_num <= 7) {
+            cout << "It's " << days[day_num - 1] << "!" << endl;
+        } else {
+            cout << "Invalid input! Please enter a number between 1 and 7." << endl;
+        }
+ } else {
+        cout << "Invalid input! Please enter a valid number between 1 and 7." << endl;
+    }
+
+    return 0;
+}
