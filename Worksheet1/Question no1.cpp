ff49e8f8265cad78021d6df8978e9c4f97a13686
Sulakshan69 <168500936+Sulakshan69@users.noreply.github.com>
2025-04-07 16:10:52 +0545

    Update Question no1.cpp

--- Worksheet1/Question no1.cpp
+++ Worksheet1/Question no1.cpp
@@ -1,3 +1,11 @@
+1.	Write a program that takes a temperature value from the user. It should then allow the user to choose between Celsius (C) and Fahrenheit (F) for conversion. After the user selection, it should then convert the entered temperature to the chosen scale and display the result.
+Use appropriate data types for temperature and handle error like non-numeric input. 
+Use the following formula for conversion:
+F = (C x 9/5) + 32
+C = (F - 32) x 5/9
+
+    
+
 #include <iostream>
 using namespace std;
 
